bc952a4a7d259d9dd451a312ece8e4586ba38acf
Ryan Whaley <ryan.whaley@stanford.edu>
2010-07-16 09:45:22 -0700

    ItpcSheet and jUnit
     * woops, forgot to include junit.jar
     * new class ItpcSheet for housing the actual data sheet and iterating Subjects
     * new unit test for the ItpcSheet class

---
+++ src/ItpcSheet.java
@@ -0,0 +1,336 @@
+/*
+ ----- BEGIN LICENSE BLOCK -----
+ Version: MPL 1.1/GPL 2.0/LGPL 2.1
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with the
+ License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ the specific language governing rights and limitations under the License.
+
+ The Original Code is PharmGen.
+
+ The Initial Developer of the Original Code is PharmGKB (The Pharmacogenetics
+ and Pharmacogenetics Knowledge Base, supported by NIH U01GM61374). Portions
+ created by the Initial Developer are Copyright (C) 2010 the Initial Developer.
+ All Rights Reserved.
+
+ Contributor(s):
+
+ Alternatively, the contents of this file may be used under the terms of
+ either the GNU General Public License Version 2 or later (the "GPL"), or the
+ GNU Lesser General Public License Version 2.1 or later (the "LGPL"), in
+ which case the provisions of the GPL or the LGPL are applicable instead of
+ those above. If you wish to allow use of your version of this file only
+ under the terms of either the GPL or the LGPL, and not to allow others to
+ use your version of this file under the terms of the MPL, indicate your
+ decision by deleting the provisions above and replace them with the notice
+ and other provisions required by the GPL or the LGPL. If you do not delete
+ the provisions above, a recipient may use your version of this file under
+ the terms of any one of the MPL, the GPL or the LGPL.
+
+ ----- END LICENSE BLOCK -----
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import util.PoiWorksheetIterator;
+
+
+/**
+ * Created by IntelliJ IDEA. User: whaleyr Date: Jun 18, 2010 Time: 10:07:11 AM To change this template use File |
+ * Settings | File Templates.
+ */
+public class ItpcSheet {
+  public static final String SHEET_NAME = "Combined_Data";
+
+  private File m_excelFile = null;
+  private Sheet m_dataSheet = null;
+
+  protected int subjectId = -1;
+  protected int projectSiteIdx = -1;
+  protected int ageIdx = -1;
+  protected int menoStatusIdx = -1;
+  protected int metastaticIdx = -1;
+  protected int erStatusIdx = -1;
+  protected int durationIdx = -1;
+  protected int tamoxDoseIdx = -1;
+  protected int tumorSourceIdx = -1;
+  protected int bloodSourceIdx = -1;
+  protected int priorHistoryIdx = -1;
+  protected int priorSitesIdx = -1;
+  protected int priorDcisIdx = -1;
+  protected int chemoIdx = -1;
+  protected int hormoneIdx = -1;
+  protected int systemicTherIdx = -1;
+  protected int followupIdx = -1;
+  protected int timeBtwSurgTamoxIdx = -1;
+  protected int firstAdjEndoTherIdx = -1;
+  protected int genoSourceIdx1 = -1;
+  protected int genoSourceIdx2 = -1;
+  protected int genoSourceIdx3 = -1;
+  protected int projectNotesIdx = -1;
+
+  protected int fluoxetineCol = -1;
+  protected int paroxetineCol = -1;
+  protected int quinidienCol = -1;
+  protected int buproprionCol = -1;
+  protected int duloxetineCol = -1;
+  protected int cimetidineCol = -1;
+  protected int sertralineCol = -1;
+  protected int citalopramCol = -1;
+
+  protected int rs4986774idx = -1;
+  protected int rs1065852idx = -1;
+  protected int rs3892097idx = -1;
+  protected int star5idx = -1;
+  protected int rs5030655idx = -1;
+  protected int rs16947idx = -1;
+  protected int rs28371706idx = -1;
+  protected int rs28371725idx = -1;
+
+  protected int amplichipidx = -1;
+
+  protected int allele1idx = -1;
+  protected int allele2idx = -1;
+  protected int allele1finalIdx = -1;
+  protected int allele2finalIdx = -1;
+  protected int callCommentsIdx = -1;
+  protected int genotypeIdx = -1;
+  protected int weakIdx = -1;
+  protected int potentIdx = -1;
+  protected int metabStatusIdx = -1;
+  protected int includeIdx = -1;
+  protected int scoreIdx = -1;
+
+  protected int incAgeIdx = -1;
+  protected int incNonmetaIdx = -1;
+  protected int incPriorHistIdx = -1;
+  protected int incErPosIdx = -1;
+  protected int incSysTherIdx = -1;
+  protected int incAdjTamoxIdx = -1;
+  protected int incDurationIdx = -1;
+  protected int incTamoxDoseIdx = -1;
+  protected int incChemoIdx = -1;
+  protected int incHormoneIdx = -1;
+  protected int incDnaCollectionIdx = -1;
+  protected int incFollowupIdx = -1;
+  protected int incGenoDataAvailIdx = -1;
+
+  private PoiWorksheetIterator m_sampleIterator = null;
+
+  /**
+   * Constructor for an ITPC data file
+   * <br/>
+   * Expectations for <code>file</code> parameter:
+   * <ol>
+   * <li>file is an Excel .XLS formatted spreadsheet</li>
+   * <li>there is a sheet in the file called "Combined_Data"</li>
+   * <li>the sheet has the first row as column headers</li>
+   * <li>the sheet has the second row as column legends</li>
+   * </ol>
+   * After this has been initialized, samples can be gathered by using the <code>getSampleIterator</code> method
+   * @param file an Excel .XLS file
+   * @throws Exception can occur from file I/O
+   */
+  public ItpcSheet(File file) throws Exception {
+    if (file == null || !file.getName().endsWith(".xls")) {
+      throw new Exception("File not in right format: " + file);
+    }
+
+    this.setExcelFile(file);
+    InputStream inputFileStream = null;
+
+    try {
+      inputFileStream = new FileInputStream(file);
+      Workbook inputWorkbook = WorkbookFactory.create(inputFileStream);
+      Sheet inputSheet = inputWorkbook.getSheet(SHEET_NAME);
+      if (inputSheet == null) {
+        throw new Exception("Cannot find worksheet named " + SHEET_NAME);
+      }
+
+      m_dataSheet = inputSheet;
+
+      parseColumnIndexes();
+
+      PoiWorksheetIterator sampleIterator = new PoiWorksheetIterator(m_dataSheet);
+      sampleIterator.next(); // skip header row
+      sampleIterator.next(); // skip legend row
+      this.setSampleIterator(sampleIterator);
+    }
+    catch (Exception ex) {
+      throw new Exception("Error initializing ITPC Sheet", ex);
+    }
+    finally {
+      if (inputFileStream != null) {
+        IOUtils.closeQuietly(inputFileStream);
+      }
+    }
+  }
+
+  protected void parseColumnIndexes() throws Exception {
+    List<String> headers = new PoiWorksheetIterator(m_dataSheet).next();
+
+    int idx = 0;
+    for (String header : headers) {
+      if (StringUtils.isNotEmpty(header)) {
+        header = header.trim().toLowerCase();
+      }
+      if (header.contains("subject id")) {
+        subjectId = idx;
+      } else if (header.contains("project site")) {
+        projectSiteIdx = idx;
+      } else if (header.contains("age at diagnosis")) {
+        ageIdx = idx;
+      } else if (header.contains("metastatic disease")) {
+        metastaticIdx = idx;
+      } else if (header.contains("menopause status at diagnosis")) {
+        menoStatusIdx = idx;
+      } else if (header.equals("estrogen receptor (existing column)")) {
+        erStatusIdx = idx;
+      } else if (header.contains("intended tamoxifen duration")) {
+        durationIdx = idx;
+      } else if (header.contains("intended tamoxifen dose")) {
+        tamoxDoseIdx = idx;
+      } else if (header.contains("if tumor or tissue was dna source")) {
+        tumorSourceIdx = idx;
+      } else if (header.contains("blood or buccal cells")) {
+        bloodSourceIdx = idx;
+      } else if (header.contains("prior history of cancer")) {
+        priorHistoryIdx = idx;
+      } else if (header.contains("sites of prior cancer")) {
+        priorSitesIdx = idx;
+      } else if (header.contains("prior invasive breast cancer or dcis")) {
+        priorDcisIdx = idx;
+      } else if (header.contains("chemotherapy")) {
+        chemoIdx = idx;
+      } else if (header.contains("additional hormone or other treatment after breast surgery? (new column)")) {
+        hormoneIdx = idx;
+      } else if (header.contains("systemic therapy prior to surgery?")) {
+        systemicTherIdx = idx;
+      } else if (header.contains("annual physical exam after breast cancer surgery")) {
+        followupIdx = idx;
+      } else if (header.contains("time between definitive breast cancer surgery")) {
+        timeBtwSurgTamoxIdx = idx;
+      } else if (header.contains("first adjuvant endocrine therapy")) {
+        firstAdjEndoTherIdx = idx;
+      } else if (header.contains("project notes (existing column)")) {
+        projectNotesIdx = idx;
+      } else if (header.contains("rs4986774")) {
+        if (!header.contains("source")) {
+          rs4986774idx = idx;
+        }
+        else {
+          genoSourceIdx1 = idx;
+        }
+      } else if (header.contains("rs1065852")) {
+        if (!header.contains("source")) {
+          rs1065852idx = idx;
+        }
+        else {
+          genoSourceIdx2 = idx;
+        }
+      } else if (header.contains("rs3892097")) {
+        if (!header.contains("source")) {
+          rs3892097idx = idx;
+        }
+        else {
+          genoSourceIdx3 = idx;
+        }
+      } else if (header.contains("rs5030655") && !header.contains("source")) {
+        rs5030655idx = idx;
+      } else if (header.contains("rs16947") && !header.contains("source")) {
+        rs16947idx = idx;
+      } else if (header.contains("rs28371706") && !header.contains("source")) {
+        rs28371706idx = idx;
+      } else if (header.contains("rs28371725") && !header.contains("source")) {
+        rs28371725idx = idx;
+      } else if (header.contains("cyp2d6 *5") && !header.contains("source")) {
+        star5idx = idx;
+      } else if (header.contains("fluoxetine")) {
+        fluoxetineCol = idx;
+      } else if (header.contains("paroxetine")) {
+        paroxetineCol = idx;
+      } else if (header.contains("quinidine")) {
+        quinidienCol = idx;
+      } else if (header.contains("buproprion")) {
+        buproprionCol = idx;
+      } else if (header.contains("duloxetine")) {
+        duloxetineCol = idx;
+      } else if (header.contains("cimetidine")) {
+        cimetidineCol = idx;
+      } else if (header.contains("sertraline")) {
+        sertralineCol = idx;
+      } else if(header.equals("citalopram (existing column)")) {
+        citalopramCol = idx;
+      } else if (header.contains("amplichip call")) {
+        amplichipidx = idx;
+      }
+
+      // new columns to add to the end of the template
+      int startPgkbColsIdx = projectNotesIdx+1;
+      allele1idx = startPgkbColsIdx;
+      allele2idx = startPgkbColsIdx + 1;
+      allele1finalIdx = startPgkbColsIdx + 2;
+      allele2finalIdx = startPgkbColsIdx + 3;
+      callCommentsIdx = startPgkbColsIdx + 4;
+      scoreIdx = startPgkbColsIdx + 5;
+      genotypeIdx = startPgkbColsIdx + 6;
+      weakIdx = startPgkbColsIdx + 7;
+      potentIdx = startPgkbColsIdx + 8;
+      metabStatusIdx = startPgkbColsIdx + 9;
+
+      incAgeIdx = startPgkbColsIdx + 10;
+      incNonmetaIdx = startPgkbColsIdx + 11;
+      incPriorHistIdx = startPgkbColsIdx + 12;
+      incErPosIdx = startPgkbColsIdx + 13;
+      incSysTherIdx = startPgkbColsIdx + 14;
+      incAdjTamoxIdx = startPgkbColsIdx + 15;
+      incDurationIdx = startPgkbColsIdx + 16;
+      incTamoxDoseIdx = startPgkbColsIdx + 17;
+      incChemoIdx = startPgkbColsIdx + 18;
+      incHormoneIdx = startPgkbColsIdx + 19;
+      incDnaCollectionIdx = startPgkbColsIdx + 20;
+      incFollowupIdx = startPgkbColsIdx + 21;
+      incGenoDataAvailIdx = startPgkbColsIdx + 22;
+
+      includeIdx = startPgkbColsIdx + 23;
+
+      idx++;
+    }
+  }
+
+  public File getExcelFile() {
+    return m_excelFile;
+  }
+
+  private void setExcelFile(File excelFile) {
+    m_excelFile = excelFile;
+  }
+
+  private PoiWorksheetIterator getSampleIterator() {
+    return m_sampleIterator;
+  }
+
+  private void setSampleIterator(PoiWorksheetIterator sampleIterator) {
+    m_sampleIterator = sampleIterator;
+  }
+
+  public boolean hasNext() {
+    return this.getSampleIterator().hasNext();
+  }
+
+  public Subject next() {
+    Subject subject = new Subject();
+
+    return subject;
+  }
+}
---
+++ test/ItpcSheetTest.java
@@ -0,0 +1,124 @@
+/*
+ ----- BEGIN LICENSE BLOCK -----
+ Version: MPL 1.1/GPL 2.0/LGPL 2.1
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with the
+ License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ the specific language governing rights and limitations under the License.
+
+ The Original Code is PharmGen.
+
+ The Initial Developer of the Original Code is PharmGKB (The Pharmacogenetics
+ and Pharmacogenetics Knowledge Base, supported by NIH U01GM61374). Portions
+ created by the Initial Developer are Copyright (C) 2010 the Initial Developer.
+ All Rights Reserved.
+
+ Contributor(s):
+
+ Alternatively, the contents of this file may be used under the terms of
+ either the GNU General Public License Version 2 or later (the "GPL"), or the
+ GNU Lesser General Public License Version 2.1 or later (the "LGPL"), in
+ which case the provisions of the GPL or the LGPL are applicable instead of
+ those above. If you wish to allow use of your version of this file only
+ under the terms of either the GPL or the LGPL, and not to allow others to
+ use your version of this file under the terms of the MPL, indicate your
+ decision by deleting the provisions above and replace them with the notice
+ and other provisions required by the GPL or the LGPL. If you do not delete
+ the provisions above, a recipient may use your version of this file under
+ the terms of any one of the MPL, the GPL or the LGPL.
+
+ ----- END LICENSE BLOCK -----
+ */
+import java.io.File;
+import junit.framework.TestCase;
+
+
+/**
+ * Created by IntelliJ IDEA. User: whaleyr Date: Jun 18, 2010 Time: 10:51:19 AM To change this template use File |
+ * Settings | File Templates.
+ */
+public class ItpcSheetTest extends TestCase {
+
+  File file = null;
+
+  public void setUp() throws Exception {
+    file = new File("/Users/whaleyr/Documents/ITPC/20100504Data/combined.xls");
+  }
+
+  public void testParseColumnIndexes() throws Exception {
+    ItpcSheet sheet = new ItpcSheet(file);
+
+    assertTrue(sheet.ageIdx>=0);
+    assertTrue(sheet.subjectId>=0);
+    assertTrue(sheet.projectSiteIdx>=0);
+    assertTrue(sheet.ageIdx>=0);
+    assertTrue(sheet.menoStatusIdx>=0);
+    assertTrue(sheet.metastaticIdx>=0);
+    assertTrue(sheet.erStatusIdx>=0);
+    assertTrue(sheet.durationIdx>=0);
+    assertTrue(sheet.tamoxDoseIdx>=0);
+    assertTrue(sheet.tumorSourceIdx>=0);
+    assertTrue(sheet.bloodSourceIdx>=0);
+    assertTrue(sheet.priorHistoryIdx>=0);
+    assertTrue(sheet.priorSitesIdx>=0);
+    assertTrue(sheet.priorDcisIdx>=0);
+    assertTrue(sheet.chemoIdx>=0);
+    assertTrue(sheet.hormoneIdx>=0);
+    assertTrue(sheet.systemicTherIdx>=0);
+    assertTrue(sheet.followupIdx>=0);
+    assertTrue(sheet.timeBtwSurgTamoxIdx>=0);
+    assertTrue(sheet.firstAdjEndoTherIdx>=0);
+    assertTrue(sheet.genoSourceIdx1>=0);
+    assertTrue(sheet.genoSourceIdx2>=0);
+    assertTrue(sheet.genoSourceIdx3>=0);
+
+    assertTrue(sheet.fluoxetineCol>=0);
+    assertTrue(sheet.paroxetineCol>=0);
+    assertTrue(sheet.quinidienCol>=0);
+    assertTrue(sheet.buproprionCol>=0);
+    assertTrue(sheet.duloxetineCol>=0);
+    assertTrue(sheet.cimetidineCol>=0);
+    assertTrue(sheet.sertralineCol>=0);
+    assertTrue(sheet.citalopramCol>=0);
+
+    assertTrue(sheet.rs4986774idx>=0);
+    assertTrue(sheet.rs1065852idx>=0);
+    assertTrue(sheet.rs3892097idx>=0);
+    assertTrue(sheet.star5idx>=0);
+    assertTrue(sheet.rs5030655idx>=0);
+    assertTrue(sheet.rs16947idx>=0);
+    assertTrue(sheet.rs28371706idx>=0);
+    assertTrue(sheet.rs28371725idx>=0);
+
+    assertTrue(sheet.amplichipidx>=0);
+
+    assertTrue(sheet.allele1idx>=0);
+    assertTrue(sheet.allele2idx>=0);
+    assertTrue(sheet.allele1finalIdx>=0);
+    assertTrue(sheet.allele2finalIdx>=0);
+    assertTrue(sheet.genotypeIdx>=0);
+    assertTrue(sheet.weakIdx>=0);
+    assertTrue(sheet.potentIdx>=0);
+    assertTrue(sheet.metabStatusIdx>=0);
+    assertTrue(sheet.includeIdx>=0);
+    assertTrue(sheet.scoreIdx>=0);
+
+    assertTrue(sheet.incAgeIdx>=0);
+    assertTrue(sheet.incNonmetaIdx>=0);
+    assertTrue(sheet.incPriorHistIdx>=0);
+    assertTrue(sheet.incErPosIdx>=0);
+    assertTrue(sheet.incSysTherIdx>=0);
+    assertTrue(sheet.incAdjTamoxIdx>=0);
+    assertTrue(sheet.incDurationIdx>=0);
+    assertTrue(sheet.incTamoxDoseIdx>=0);
+    assertTrue(sheet.incChemoIdx>=0);
+    assertTrue(sheet.incHormoneIdx>=0);
+    assertTrue(sheet.incDnaCollectionIdx>=0);
+    assertTrue(sheet.incFollowupIdx>=0);
+    assertTrue(sheet.incGenoDataAvailIdx>=0);
+  }
+}
